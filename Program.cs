8b9337062e694a01322893f0f5fd19c2b59640b4
Marlyson Mendes <marlysonmendes@gmail.com>
2022-03-12 16:48:00 -0300

    Conectar redis e models

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<IConnectionMultiplexer>(opt => 
+    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("DockerRedisConnection")));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
